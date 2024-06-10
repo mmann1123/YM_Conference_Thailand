d61158074120831e034e258293b851c14dc43ebe
Michael Mann <mmann1123@gmail.com>
2024-06-09 23:35:07 -0400

    add TZ data

--- data_prep.py
+++ data_prep.py
@@ -97,4 +97,85 @@ points.to_file(
     r"/mnt/bigdrive/Dropbox/Presentations/YouthMappers_Thailand_2024/Tanzania/tz_labels.geojson",
     driver="GeoJSON",
 )
+# %%  Tanzania
+
+import geopandas as gpd
+
+bbox = gpd.read_file(
+    "/home/mmann1123/Dropbox/Presentations/YouthMappers_Thailand_2024/Tanzania/bbox.gpkg",
+)
+bbox.explore()
+
+# %%
+import pandas as pd
+
+a = gpd.read_file(
+    "/home/mmann1123/Dropbox/Tanzania_data/Projects/YM_Tanzania_Field_Boundaries/kobo_field_collections/combined_data_reviewed_xy_LC_RPN_Final.shp"
+)[["primar", "geometry"]]
+a.columns = ["lc_name", "geometry"]
+b = gpd.read_file(
+    "/home/mmann1123/Dropbox/Tanzania_data/Projects/YM_Tanzania_Field_Boundaries/kobo_field_collections/other_training.gpkg"
+)[["lc_name", "geometry"]]
+b.columns = ["lc_name", "geometry"]
+c = gpd.read_file(
+    "/home/mmann1123/Dropbox/Tanzania_data/Projects/YM_Tanzania_Field_Boundaries/kobo_field_collections/exported_classified_points.geojson"
+)[["landCover", "geometry"]]
+c.columns = ["lc_name", "geometry"]
+
+lc = pd.concat([a, b, c])
+lc
+
+# %%
+set(lc.lc_name)
+# %%
+
+keep = [
+    "rice",
+    "maize",
+    "cassava",
+    "sunflower",
+    "sorghum",
+    "urban",
+    "forest",
+    "shrub",
+    "tidal",
+    "cotton",
+    "water",
+    "fallow_barren",
+    "forest_shrubland",
+    "millet",
+    "large_building",
+    "water_body",
+]
+drop = [
+    "Don't know",
+    "Other (later, specify in optional notes)",
+    "could be maize.",
+    "no",
+    "don_t_know",
+]
+
+# apply keep/drop
+lc.drop(lc[lc["lc_name"].isin(drop)].index, inplace=True)
+lc.loc[lc["lc_name"].isin(keep) == False, "lc_name"] = "Other"
+lc.reset_index(drop=True, inplace=True)
+
+lc.replace(
+    {"water_body": "water", "large_building": "urban", "Other": "other"}, inplace=True
+)
+set(lc.lc_name)
+# %%
+
+lc = lc.clip(bbox)
+from sklearn.preprocessing import LabelEncoder
+
+le = LabelEncoder
+lc["lc_code"] = le().fit_transform(lc.lc_name)
+lc.explore()
+# %%
+lc.to_file(
+    "/home/mmann1123/Dropbox/Presentations/YouthMappers_Thailand_2024/Tanzania/lc_classes.geojson",
+    driver="GeoJSON",
+)
+
 # %%
